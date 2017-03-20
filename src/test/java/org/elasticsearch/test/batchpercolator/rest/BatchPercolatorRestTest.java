70458fd7c8825a9e81129ad678487a8ef91a577c
Pelle Berglund <per.berglund@meltwater.com>
2017-03-20 18:49:44 +0000

    Make RestTest deterministic
    - Use JSONPath

--- src/test/java/org/elasticsearch/test/batchpercolator/rest/BatchPercolatorRestTest.java
+++ src/test/java/org/elasticsearch/test/batchpercolator/rest/BatchPercolatorRestTest.java
@@ -1,27 +1,38 @@
 package org.elasticsearch.test.batchpercolator.rest;
 
+import com.jayway.jsonpath.Filter;
+import com.jayway.jsonpath.JsonPath;
 import com.meltwater.elasticsearch.action.BatchPercolateSourceBuilder;
 import com.meltwater.elasticsearch.index.BatchPercolatorService;
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.Response;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.collect.ImmutableList;
+import org.elasticsearch.common.collect.ImmutableMap;
+import org.elasticsearch.common.collect.Lists;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.search.highlight.HighlightBuilder;
 import org.elasticsearch.test.integration.AbstractNodesTests;
+import org.hamcrest.CoreMatchers;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import static com.jayway.jsonpath.Criteria.where;
+import static com.jayway.jsonpath.Filter.filter;
 import static com.meltwater.elasticsearch.action.BatchPercolateSourceBuilder.docBuilder;
 import static org.elasticsearch.common.xcontent.ToXContent.EMPTY_PARAMS;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
-import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
@@ -44,6 +55,7 @@ public class BatchPercolatorRestTest extends AbstractNodesTests {
     public void basicRestPercolationTest() throws ExecutionException, InterruptedException, IOException {
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 
+        final String docId = "docId";
         logger.info("--> Add dummy doc");
         client.admin().indices().prepareDelete("_all").execute().actionGet();
         client.prepareIndex("test", "type", "1")
@@ -67,10 +79,11 @@ public class BatchPercolatorRestTest extends AbstractNodesTests {
                 .execute().actionGet();
 
         logger.info("--> Doing percolation with Rest API");
+
         BytesReference source = new BatchPercolateSourceBuilder().addDoc(
                 docBuilder().setDoc(jsonBuilder()
                         .startObject()
-                        .field("_id", "docId")
+                        .field("_id", docId)
                         .field("field1", "the fox is here")
                         .field("field2", "meltwater percolator")
                         .endObject()))
@@ -84,9 +97,21 @@ public class BatchPercolatorRestTest extends AbstractNodesTests {
 
 
         assertThat(restResponse.getStatusCode(), equalTo(200));
-        //TODO would be nicer to map to a Java object.
-        assertThat(restResponse.getResponseBody(), endsWith(
-                "\"_shards\":{\"total\":5,\"successful\":5,\"failed\":0},\"results\":[{\"doc\":\"docId\",\"matches\":[{\"query_id\":\"1\",\"highlights\":{\"field1\":[\"the <b>fox</b> is here\"]}},{\"query_id\":\"2\",\"highlights\":{\"field2\":[\"<b>meltwater</b> percolator\"]}}]}]}"));
+
+        String responseBody = restResponse.getResponseBody();
+
+
+        List<String> results = JsonPath.read(responseBody, "$.results");
+        assertThat(results.size(), is(1));
+        String matchedDoc = JsonPath.read(responseBody, "$.results[0].doc");
+        assertThat(matchedDoc, is(docId));
+        List<String> matches = JsonPath.read(responseBody, "$.results[0].matches");
+        assertThat(matches.size(), is(2));
+
+        assertThat(JsonPath.<List<String>>read(responseBody, "$.results[0].matches[?(@.query_id==1)].query_id").get(0), is("1"));
+        assertThat(JsonPath.<List<String>>read(responseBody, "$.results[0].matches[?(@.query_id==1)].highlights.field1[0]").get(0), is("the <b>fox</b> is here"));
+        assertThat(JsonPath.<List<String>>read(responseBody, "$.results[0].matches[?(@.query_id==2)].query_id").get(0), is("2"));
+        assertThat(JsonPath.<List<String>>read(responseBody, "$.results[0].matches[?(@.query_id==2)].highlights.field2[0]").get(0), is("<b>meltwater</b> percolator"));
 
     }
 
